8c96d954d48ec3b4d955d3c70f04e3bcddf62781
DanielBonder <danielbonder123@gmail.com>
2025-01-30 15:32:46 +0200

    sql
    sql

---
+++ qu.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    full_name VARCHAR(255) NOT NULL,
+    username VARCHAR(50) UNIQUE,
+    email VARCHAR(100) UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    weight DECIMAL(5,2) NOT NULL,
+    height DECIMAL(5,2) NOT NULL,
+    bmi DECIMAL(5,2) GENERATED ALWAYS AS (weight / ((height / 100) * (height / 100))) STORED,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
