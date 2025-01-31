d157cf887b3454ff1d0765335dab4d24d3c86f2c
DanielBonder <danielbonder123@gmail.com>
2025-01-31 12:16:44 +0200

    תיקון
    טעות

--- qu.sql
+++ qu.sql
@@ -1,8 +1,11 @@
-CREATE TABLE IF NOT EXISTS users (
+CREATE DATABASE `Db_Management_App`;
+USE `Db_Management_App`;
+
+CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     full_name VARCHAR(255) NOT NULL,
-    username VARCHAR(50) UNIQUE,
-    email VARCHAR(100) UNIQUE,
+    username VARCHAR(50),
+    email VARCHAR(100),
     password VARCHAR(255) NOT NULL,
     weight DECIMAL(5,2) NOT NULL,
     height DECIMAL(5,2) NOT NULL,
